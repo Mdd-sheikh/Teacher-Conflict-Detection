61b54640e7d01f92b8f0e4b5be022d2bd93f3281
Your Name <your-email@example.com>
2026-05-07 11:35:41 -0700

    add user model

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+
+// db connection
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
