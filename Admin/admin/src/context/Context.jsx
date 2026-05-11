80f0604c63bf4b43ce2b06e8485f0a468fb70d3d
Your Name <your-email@example.com>
2026-05-11 00:12:34 -0700

    add dist file

--- Admin/admin/src/context/Context.jsx
+++ Admin/admin/src/context/Context.jsx
@@ -4,9 +4,9 @@ export const Context = createContext(null)
 
 
 const ContextProvider = ({ children }) => {
-    
+    const name = ""
     const value = {
-
+        name
     }
 
     return <Context.Provider value={value}>
