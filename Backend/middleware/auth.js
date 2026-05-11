558f63243956d7264ebd2b49c732d88975f54494
Your Name <your-email@example.com>
2026-05-11 06:13:31 -0700

    fix this auth problem

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import UserModel from "../models/userModel.js";
+import UserModel from "../Models/useModelr.js";
 
 export const auth = async (req, res, next) => {
 
